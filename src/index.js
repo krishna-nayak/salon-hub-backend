5d3ac733698d338859b92e643a0e6bec47a15020
Krishna <nayak100krishna@gmail.com>
2024-01-05 09:39:29 +0530

    database connection ✅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// import { sequelize } from "./db/models";
+const { sequelize, User } = require("./db/models");
+const { where } = require("sequelize");
+// const { User } = require("./db/models");
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -9,10 +14,47 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+/* *********************************************************************************** */
+
+// GET ALL USER
+app.get("/users", async (req, res) => {
+  try {
+    const user = await User.findAll();
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+app.get("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await User.findOne({ where: { userId } });
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// Add new User (CREATE)
+app.post("/users", async (req, res) => {
+  const { fullName, email, role, password } = req.body;
+  try {
+    const user = await User.create({ fullName, email, role, password });
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+/* *********************************************************************************** */
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () =>
-  console.log(`Server running on: http://localhost:${PORT}`)
-);
+app.listen(PORT, async () => {
+  await sequelize.authenticate();
+  console.log(`Server running on: http://localhost:${PORT}`);
+});
