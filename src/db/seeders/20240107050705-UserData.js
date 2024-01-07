29c5226f16b109ba41c9d0df083c52c1d40e1221
Krishna <nayak100krishna@gmail.com>
2024-01-07 14:01:58 +0530

    seed:dataInput

---
+++ src/db/seeders/20240107050705-UserData.js
@@ -0,0 +1,78 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    return await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          userId: "f57ba5ea-3dbe-4f88-aab6-95ef83b6b7d8",
+          fullName: "Michel Massel",
+          email: "mmassel0@blog.com",
+          password: "mZ5_6/6D~,",
+          role: "admin",
+          createdAt: "8/8/2023",
+          updatedAt: "4/21/2023",
+        },
+        {
+          userId: "5081beac-1d5a-4f08-ad55-2face04e362a",
+          fullName: "Ardith Surplice",
+          email: "asurplice1@senate.gov",
+          password: "eA6'gV(tC5Pedj.",
+          role: "customer",
+          createdAt: "11/9/2023",
+          updatedAt: "6/11/2023",
+        },
+        {
+          userId: "d8a0acea-015c-4da9-9d5c-a0f7777fd0af",
+          fullName: "Gus Seamen",
+          email: "gseamen2@topsy.com",
+          password: "vV3>bYSmL",
+          role: "customer",
+          createdAt: "6/18/2023",
+          updatedAt: "6/26/2023",
+        },
+        {
+          userId: "f1d2ca63-a8e5-4fed-8f8d-f5dba8c2a10c",
+          fullName: "Elsa Salmen",
+          email: "esalmen3@baidu.com",
+          password: "yI7>>3\\>Zp7",
+          role: "admin",
+          createdAt: "3/22/2023",
+          updatedAt: "7/6/2023",
+        },
+        {
+          userId: "7232f11f-b5dc-47d0-ba14-8f2bbe3afb02",
+          fullName: "Kiel Bowkley",
+          email: "kbowkley4@jimdo.com",
+          password: 'pC5"bn%+',
+          role: "admin",
+          createdAt: "2/9/2023",
+          updatedAt: "10/8/2023",
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return await queryInterface.bulkDelete("users", null, {});
+  },
+};
---
+++ src/db/seeders/20240107052231-SalonData.js
@@ -0,0 +1,80 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+const data = [
+  {
+    salonid: "80cf4156-a27a-45de-939a-c840c4b4ebc0",
+    name: "Zak Felstead",
+    address: "Suite 61",
+    city: "Hujia",
+    openinghourstart: "23:16",
+    closeingHour: "8:17",
+    createdAt: "7/18/2023",
+    updatedAt: "6/19/2023",
+  },
+  {
+    salonid: "b783787b-77dd-4bd9-b3a4-8624fbf1081d",
+    name: "Ulberto Stovine",
+    address: "Apt 1224",
+    city: "Irshava",
+    openinghourstart: "3:16",
+    closeingHour: "8:00",
+    createdAt: "1/28/2023",
+    updatedAt: "5/4/2023",
+  },
+  {
+    salonid: "49c5873a-4a54-43f9-b2a2-d9c41471c02c",
+    name: "Faun Holton",
+    address: "Suite 73",
+    city: "Chongkan",
+    openinghourstart: "15:25",
+    closeingHour: "4:53",
+    createdAt: "3/11/2023",
+    updatedAt: "9/22/2023",
+  },
+  {
+    salonid: "e6be4fd5-ace6-412c-a494-ae1378d04ade",
+    name: "Rutter Korneluk",
+    address: "PO Box 99742",
+    city: "Talun",
+    openinghourstart: "14:07",
+    closeingHour: "0:12",
+    createdAt: "5/20/2023",
+    updatedAt: "12/30/2023",
+  },
+  {
+    salonid: "b2df36e0-cc54-4af8-bc4f-df311afff8de",
+    name: "Horten Fisbey",
+    address: "Apt 69",
+    city: "Somorpenang",
+    openinghourstart: "3:14",
+    closeingHour: "0:42",
+    createdAt: "8/16/2023",
+    updatedAt: "2/14/2023",
+  },
+];
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    return await queryInterface.bulkInsert("salons", data, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return await queryInterface.bulkDelete("salons", null, {});
+  },
+};
