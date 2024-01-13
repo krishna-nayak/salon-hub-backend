07989012f09b104ac123231680badaa35dae46c3
Krishna <nayak100krishna@gmail.com>
2024-01-13 18:25:53 +0530

    refactor - code.

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -12,6 +12,7 @@ const db = {};
 let sequelize;
 console.log(process.env.PSQL_HOST);
 console.log(config);
+console.log(env);
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
