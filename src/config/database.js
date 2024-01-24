16a30f463239e987a7a2f4bacdd886382468e33e
Krishna <nayak100krishna@gmail.com>
2024-01-25 00:26:05 +0530

    new configuration

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,13 @@ module.exports = {
     host: process.env.PSQL_HOST,
     dialect: "postgres",
   },
+  docker_development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.PSQL_HOST,
+    dialect: "postgres",
+  },
   test: {
     username: "root",
     password: null,
@@ -14,10 +21,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE_DEV,
+    host: process.env.PSQL_HOST,
+    dialect: "postgres",
   },
 };
