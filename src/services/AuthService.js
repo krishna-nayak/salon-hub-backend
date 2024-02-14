fd43ea3571744fc2a293914f77c1fc3e0cf8a9b2
Krishna <nayak100krishna@gmail.com>
2024-02-14 17:41:48 +0530

    auth aervices

---
+++ src/services/AuthService.js
@@ -0,0 +1,23 @@
+const UserError = require("../ErrorHandler/UserError");
+var db = require("../db/models");
+
+var User = db.User;
+
+class AuthService {
+  static async login(email, password) {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      const msg = "User not found";
+      const extra = "Please check your email address: NO such email exist";
+      throw new UserError(msg, 401, extra);
+    }
+    if (user.password !== password) {
+      const msg = "Password is incorrect";
+      const extra = "Please use the correct Password";
+      throw new UserError(msg, 401, extra);
+    }
+    return [user, 200];
+  }
+}
+
+module.exports = AuthService;
